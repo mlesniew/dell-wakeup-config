3244dcf96efd320f420128ec80dd00af1a3b25b6
Michał Leśniewski <mlesniew@gmail.com>
2024-03-21 16:59:48 +0100

    Extend wakeup whitelist to all INTC* devices

--- configure_wakeup.sh
+++ configure_wakeup.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
 grep -Fl enabled /sys/bus/*/devices/*/power/wakeup |
-	grep -vF '/INTC1070:00/' |
+	grep -vE '/INTC[0-9]+:[0-9]+/' |
 	grep -vF '/thunderbolt/' |
 	xargs -I% bash -c 'echo 'disabled' > %'
